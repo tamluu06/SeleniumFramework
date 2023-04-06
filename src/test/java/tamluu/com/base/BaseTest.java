d6a1c0283cd222a2a932274c954c94d61e308c58
Tam Luu <tamluu06@gmail.com>
2023-04-06 10:00:27 -0600

    update

--- src/test/java/tamluu/com/base/BaseTest.java
+++ src/test/java/tamluu/com/base/BaseTest.java
@@ -7,9 +7,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.*;
-import tamluu.com.listeners.TestListener;
-@Listeners(TestListener.class)
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+
+
 public class BaseTest {
 
     @BeforeMethod
